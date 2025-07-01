f090a703ca18ed16333436a412001becbcc579ac
Edgar Eler <eler@edgar.systems>
2025-07-01 15:10:18 -0300

    Configure logger

---
+++ src/logger/logger.mjs
@@ -0,0 +1,60 @@
+import logsAPI from '@opentelemetry/api-logs';
+import {
+  LoggerProvider,
+  SimpleLogRecordProcessor,
+} from '@opentelemetry/sdk-logs';
+import { OTLPLogExporter } from '@opentelemetry/exporter-logs-otlp-http';
+import { OpenTelemetryTransportV3 } from '@opentelemetry/winston-transport';
+import { resourceFromAttributes } from '@opentelemetry/resources';
+import {
+  ATTR_SERVICE_NAME,
+  ATTR_SERVICE_VERSION,
+} from '@opentelemetry/semantic-conventions';
+import { ATTR_DEPLOYMENT_ENVIRONMENT_NAME } from '@opentelemetry/semantic-conventions/incubating';
+import winston from 'winston';
+
+const serviceName = process.env.DEPLOYMENT_ENVIRONMENT;
+const environment = process.env.DEPLOYMENT_ENVIRONMENT;
+
+const otlpExplorer = new OTLPLogExporter({
+  url: process.env.OTEL_EXPORTER_OTLP_ENDPOINT,
+  headers: {
+    'signoz-access-token': process.env.SIGNOZ_INGESTION_KEY,
+  },
+});
+
+const loggerProvider = new LoggerProvider({
+  resource: resourceFromAttributes({
+    [ATTR_SERVICE_NAME]: serviceName,
+    [ATTR_SERVICE_VERSION]: process.env.SERVICE_VERSION,
+    [ATTR_DEPLOYMENT_ENVIRONMENT_NAME]: environment,
+  }),
+  processors: [new SimpleLogRecordProcessor(otlpExplorer)],
+});
+
+logsAPI.logs.setGlobalLoggerProvider(loggerProvider);
+
+export const logger = winston.createLogger({
+  level: 'debug',
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.errors({
+      stack: true,
+    }),
+    winston.format.metadata(),
+    winston.format.json()
+  ),
+  defaultMeta: {
+    service: serviceName,
+    environment,
+  },
+  transports: [
+    new OpenTelemetryTransportV3({
+      loggerProvider,
+      logAttributes: {
+        [ATTR_SERVICE_NAME]: serviceName,
+        [ATTR_DEPLOYMENT_ENVIRONMENT_NAME]: environment,
+      },
+    }),
+  ],
+});
